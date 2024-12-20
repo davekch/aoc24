0aa48ebdfecd507e5cf2d3c879a6af13a04d536d
davekch <dvkch@proton.me>
2024-12-20 19:22:26 +0100

    day 20 rewrite for part 2

--- day20/solver.py
+++ day20/solver.py
@@ -25,26 +25,37 @@ def parse(raw_data):
         elif char == "E":
             end = point
             grid[point] = "."
-    return Graph(grid), start, end
+    graph = Graph(grid)
+    path = BFS(graph, start, finished=lambda p: p == end)
+    return shortestpath(path, start, end)
+
+
+def count_cheats(path, i, distance):
+    saved = Counter()  # counts number of picoseconds saved
+    cheat_start = path[i]
+    # get all points that are distance away from start
+    for k in range(-distance, distance+1):
+        for l in range(-(distance-abs(k)), distance-abs(k)+1):
+            # ignore direct neighbors, no cheating needed for them
+            if abs(k) + abs(l) > 1:
+                cheat_end = Vec(cheat_start.x + k, cheat_start.y + l)
+                if cheat_end in path:
+                    # see if it's further down the path than we are now
+                    # and if it saves time
+                    j = path.index(cheat_end)
+                    if j > i:
+                        time_saved = j - i - abs(k) - abs(l)
+                        saved[time_saved] += 1
+    return saved
 
 
 @watch.measure_time
 def solve1(data):
-    graph, start, end = data
-    path = BFS(graph, start, finished=lambda p: p == end)
-    best_no_cheating = len(shortestpath(path, start, end))
     cheat_saves = Counter()
     from tqdm import tqdm
-    for point in tqdm(graph.graph):
-        if graph.graph[point] != "#":
-            continue
-        # disable this wall and do BFS again
-        graph.graph[point] = "."
-        path = BFS(graph, start, finished=lambda p: p == end)
-        best_cheating = len(shortestpath(path, start, end))
-        cheat_saves[best_no_cheating - best_cheating] += 1
-        # add wall back in again
-        graph.graph[point] = "#"
+    for i in tqdm(range(len(data))):
+        cheat_saves += count_cheats(data, i, 2)
+    # print(cheat_saves)
     result = 0
     for save, n in cheat_saves.items():
         if save >= 100:
@@ -52,10 +63,18 @@ def solve1(data):
     return result
 
 
-
 @watch.measure_time
 def solve2(data):
-    pass
+    cheat_saves = Counter()
+    from tqdm import tqdm
+    for i in tqdm(range(len(data))):
+        cheat_saves += count_cheats(data, i, 20)
+    result = 0
+    for save, n in cheat_saves.items():
+        if save >= 100:
+            # print(f"{n} cheats to save {save}")
+            result += n
+    return result
 
 
 if __name__ == "__main__":
