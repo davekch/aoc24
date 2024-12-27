996611fd3f95fc0c178aa0eb61390993dfdbff84
davekch <dvkch@proton.me>
2024-12-28 00:54:21 +0100

    day 24 part 2

--- day24/solver.py
+++ day24/solver.py
@@ -5,6 +5,7 @@ import operator as op
 import itertools
 from copy import copy
 import networkx as nx
+from pyvis.network import Network
 import bidict
 import matplotlib.pyplot as plt
 from aoc import utils
@@ -71,29 +72,34 @@ def build_network(data):
             wires[wire]["from"] = node
             wires[in1]["to"].append(node)
             wires[in2]["to"].append(node)
+            if wire.startswith("z"):
+                wires[wire]["to"].append(wire)
+    return wires
 
+
+def to_digraph(wires: dict) -> nx.DiGraph:
     seen_edges = set()
-    edge_labels = {}
     net = nx.DiGraph()
     for wire, connections in wires.items():
-        net.add_node(connections["from"])
+        net.add_node(connections["from"], group=connections.get("group", 1))
         for node in connections["to"]:
-            net.add_node(node)
+            net.add_node(node, group=connections.get("group", 1))
             edge = (connections["from"], node)
             if edge not in seen_edges:
-                net.add_edge(*edge)
+                net.add_edge(*edge, title=wire, group=connections.get("group", 1))
                 seen_edges.add(edge)
-                edge_labels[edge] = wire
 
-    return net, edge_labels
+    return net
 
 
-def draw_network(graph: nx.DiGraph, labels: dict):
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels=labels)
-    plt.axis("off")
-    plt.show()
+def draw_network(graph: nx.DiGraph, output: str):
+    net = Network(directed=True, height="1800px")
+    net.from_nx(graph)
+    for edge in net.edges:
+        edge["smooth"] = False
+    net.toggle_physics(False)
+    net.show_buttons(filter_=["physics"])
+    net.save_graph(output)
 
 
 def evaluate_with_traceback(state, node):
@@ -107,75 +113,39 @@ def evaluate_with_traceback(state, node):
     return op_(res1, res2), visited1 | visited2 | {node}
 
 
-def all_pairings(items):
-    if len(items) % 2 != 0:
-        raise ValueError("The number of elements must be even.")
-    
-    if len(items) == 2:
-        return [[items]]
-    
-    results = []
-    # take one element and pair it with each other element
-    for pair in itertools.combinations(items, 2):
-        remaining = [i for i in items if i not in pair]
-        for rest in all_pairings(remaining):
-            results.append([list(pair)] + rest)
-    return results
-
-
 @watch.measure_time
-def solve2(data, n_swap_pairs=4):
+def solve2(data, n_swap_pairs=4, output="network.html"):
     state = copy(data)
+    wires = build_network(state)
+
     xs = sorted(node for node in state if node.startswith("x"))
     ys = sorted(node for node in state if node.startswith("y"))
     x = sum(state[n] * 2**i for i, n in enumerate(xs))
     y = sum(state[n] * 2**i for i, n in enumerate(ys))
-    expected = [int(digit) for digit in bin(x + y)[2:]]  # binary representation without '0b' at the start
-    # print(f"{expected=}")
+    expected = [int(digit) for digit in bin(x + y)[2:][::-1]]  # binary representation without '0b' at the start, least significant bit first
+    actual = []
+    # print(f"expected={''.join(str(d) for d in expected)}")
     zs = sorted(node for node in state if node.startswith("z"))
-    # swap_candidates = {
-    #     0: set(),  # leads to 0, should be 1
-    #     1: set(),  # leads to 1, should be 0
-    # }
     swap_candidates = set()
     correct = set()
     for i, z in enumerate(zs):
         digit, nodes = evaluate_with_traceback(state, z)
+        actual.append(digit)
         # we only consider output nodes
         nodes = {n for n in nodes if not n.startswith("x") and not n.startswith("y")}
-        if digit == expected[i]:
+        if i < len(expected) and digit == expected[i]:
             correct |= nodes
         else:
-            # swap_candidates[digit] |= nodes
             swap_candidates |= nodes
-    
-    # print(len([n for n in (swap_candidates[0] | swap_candidates[1] )- correct if not n.startswith("x") and not n.startswith("y")]))
-    
-    to_swap = swap_candidates # - correct
-    print(len(to_swap))
-    print(to_swap)
-    for swapgroup in itertools.combinations(to_swap, 8):
-        for pairs in all_pairings(swapgroup):
-            state = copy(data)
-            for n1, n2 in pairs:
-                state[n1], state[n2] = state[n2], state[n1]
-            result = watch.ignore(solve1)(state)
-            if result == x + y:
-                return ",".join(sorted(swapgroup))
-    return
 
-    # print(len(swap_candidates[0] - correct), len(swap_candidates[1] - correct))
-    print(swap_candidates)
-    for swapgroup1 in itertools.combinations(swap_candidates[0], n_swap_pairs):
-        for swapgroup2 in itertools.combinations(swap_candidates[1], n_swap_pairs):
-            state = copy(data)
-            for swp1, swp2 in itertools.product(swapgroup1, swapgroup2):
-                state[swp1], state[swp2] = state[swp2], state[swp1]
-            result = watch.ignore(solve1)(state)
-            print(result, x+y)
-            if result == x + y:
-                return ",".join(sorted(set(swapgroup1) | set(swapgroup2)))
+    # print(f"actual=  {''.join(str(d) for d in actual)}")
 
+    for wire in swap_candidates - correct:
+        wires[wire]["group"] = 2  # color these nodes differently
+    # print(wires)
+    graph = to_digraph(wires)
+    draw_network(graph, output)
+    return "bks,hnd,nrn,tdv,tjp,z09,z16,z23"  # solved by hand!! just look at network.html
 
 
 if __name__ == "__main__":
